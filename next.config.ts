bb113e73e621108de5b4c904ac0a851315fb2878
chos1909@gmail.com <chos1909@gmail.com>
2025-05-23 11:52:08 +0900

    rewrite 임시 제거거

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,16 @@ const nextConfig: NextConfig = {
   // output:'export',
   // assetPrefix : 'https://s3.ap-northeast-2.amazonaws.com/purfit.co.kr/out/',
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',       // 프론트에서 /api/로 시작하는 요청
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',       // 프론트에서 /api/로 시작하는 요청
+  //       destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`
 
-        // destination: process.env.NEXT_PUBLIC_ENV ==='develop' ? 'http://localhost:8088/api/:path*' : , // 백엔드로 프록시
-      },
-    ];
-  },
+  //       // destination: process.env.NEXT_PUBLIC_ENV ==='develop' ? 'http://localhost:8088/api/:path*' : , // 백엔드로 프록시
+  //     },
+  //   ];
+  // },
   images : {
     domains: [
       'purfit.co.kr.s3.ap-northeast-2.amazonaws.com',
